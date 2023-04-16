5539fc38f67d8f86ec03868dd1a462d861e68029
DipokSarker <dipok.ronjan.sarker@gmail.com>
2023-04-16 23:25:46 +0300

    bookcreate added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-    return <div>App</div>
-}
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
 
-export default App;
+function App() {
+    const [books, setBooks] = useState([]);
+
+    const createBook = (title) => {
+        console.log('Need to add book with:', title);
+
+    };
+    
+    return (
+        <div>
+        <BookCreate onCreate={createBook} />
+    </div>
+    );
+   }
+
+export default App;;
